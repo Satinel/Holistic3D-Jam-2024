39c578c0f360e4b41a941d341d43966fb8cf8e31
Nimufu <angelonmyknees@gmail.com>
2024-08-12 04:44:51 -0300

    CoinSend script to finally split CurrencyButton into the two scripts it should have been to start with

---
+++ Assets/Scripts/Inventory/CoinSend.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CoinSend : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] DropBox _coinBox;
+    [SerializeField] Button _increaseButton;
+    [SerializeField] Currency _currency;
+
+    [SerializeField] int _sendAmount;
+
+    bool _isOver, _isCustomer;
+
+    void OnEnable()
+    {
+        TradingSystem.OnNewCustomer += TradingSystem_OnNewCustomer;
+    }
+
+    void OnDisable()
+    {
+        TradingSystem.OnNewCustomer -= TradingSystem_OnNewCustomer;
+        _isOver = false;
+    }
+
+    void Update()
+    {
+        if(!_isCustomer) { return; }
+        if(!_isOver) { return; }
+
+        if(Input.mouseScrollDelta.y > 0)
+        {
+            if(!_increaseButton) { return; }
+
+            _increaseButton.onClick.Invoke();
+        }
+
+        if(Input.mouseScrollDelta.y < 0)
+        {
+            _coinBox.RetrieveItem(_currency);
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            if(_coinBox)
+            {
+                _coinBox.SendXChildItems(transform, _sendAmount);
+            }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _isOver = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _isOver = false;
+    }
+
+    void TradingSystem_OnNewCustomer(Customer customer)
+    {
+        if(!customer)
+        {
+            _isCustomer = false;
+            return;
+        }
+
+        if(customer.CustomerType == Customer.Type.None)
+        {
+            _isCustomer = false;
+        }
+        else
+        {
+            _isCustomer = true;
+        }
+    }
+}
--- Assets/Scripts/Trading/CurrencyButton.cs
+++ Assets/Scripts/Trading/CurrencyButton.cs
@@ -4,28 +4,18 @@ using UnityEngine.UI;
 
 public class CurrencyButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [SerializeField] DropBox _coinBox;
     [SerializeField] Button _increaseButton, _decreaseButton;
     [SerializeField] Currency _currency;
 
-    [SerializeField] int _sendAmount;
-
-    bool _isOver, _isCustomer;
-
-    void OnEnable()
-    {
-        TradingSystem.OnNewCustomer += TradingSystem_OnNewCustomer;
-    }
+    bool _isOver;
 
     void OnDisable()
     {
-        TradingSystem.OnNewCustomer -= TradingSystem_OnNewCustomer;
         _isOver = false;
     }
 
     void Update()
     {
-        if(!_isCustomer) { return; }
         if(!_isOver) { return; }
 
         if(Input.mouseScrollDelta.y > 0)
@@ -37,22 +27,9 @@ public class CurrencyButton : MonoBehaviour, IPointerEnterHandler, IPointerExitH
 
         if(Input.mouseScrollDelta.y < 0)
         {
-            if(_decreaseButton && !_coinBox)
-            {
-                _decreaseButton.onClick.Invoke();
-            }
-            else if(_coinBox)
-            {
-                _coinBox.RetrieveItem(_currency);
-            }
-        }
+            if(!_decreaseButton) { return; }
 
-        if(Input.GetMouseButtonDown(1))
-        {
-            if(_coinBox)
-            {
-                _coinBox.SendXChildItems(transform, _sendAmount);
-            }
+            _decreaseButton.onClick.Invoke();
         }
     }
 
@@ -64,24 +41,5 @@ public class CurrencyButton : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     public void OnPointerExit(PointerEventData eventData)
     {
         _isOver = false;
-    }
-
-    void TradingSystem_OnNewCustomer(Customer customer)
-    {
-        if(!customer)
-        {
-            _isCustomer = false;
-            return;
-        }
-
-        if(customer.CustomerType == Customer.Type.None)
-        {
-            _isCustomer = false;
-        }
-        else
-        {
-            _isCustomer = true;
-        }
-
     }
 }
